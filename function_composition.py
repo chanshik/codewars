9d52f71ee0e58b2599141e1e6ce37a2e41189667
chanshik <chanshik@gmail.com>
2015-03-13 00:46:46 +0900

    Add functional composition.

---
+++ function_composition.py
@@ -0,0 +1,35 @@
+"""
+Function composition is a mathematical operation that
+mainly presents itself in lambda calculus and computability.
+It is explained well here, but this is my explanation, in simple mathematical notation:
+
+f3 = compose( f1 f2 )
+   Is equivalent to...
+f3(a) = f1( f2( a ) )
+
+Your task is to create a compose function to carry out this task,
+which will be passed two functions or lambdas.
+Ruby functions will be passed, and should return,
+either a proc or a lambda.
+Remember that the resulting composed function may be passed multiple arguments!
+
+compose(f , g)(x)
+=> f( g( x ) )
+"""
+
+
+def compose(f, g):
+    def wrapper(*args):
+        return f(g(*args))
+
+    return wrapper
+
+from unittest import TestCase
+
+
+class TestFunctionComposition(TestCase):
+    def test_compose(self):
+        add1 = lambda a: a + 1
+        this = lambda a: a
+
+        self.assertEquals(1, compose(add1, this)(0))
